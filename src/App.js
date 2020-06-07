d5c339ad561f4634aa92e4ce9a45717a642b87f9
huzaifa-aslam <huzaifaslam45@gmail.com>
2020-06-07 16:51:37 +0500

    files added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > hello world im huzaifa aslam < /h1> <
-        /div>
+    return (
+        hello - world
     );
 }
 
